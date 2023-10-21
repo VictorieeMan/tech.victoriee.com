9eb55b064c8f080887f6ced4e2119ea1986386ca
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-10-21 10:32:50 +0200

    code cleanup

--- blog-site/docs/javascripts/mash.js
+++ blog-site/docs/javascripts/mash.js
@@ -1,14 +1,17 @@
 // Mash: https://app.mash.com
 window.MashSettings = {
-id: "56fe16a5-4f88-4944-8355-de30f80b51d9"
+    id: "56fe16a5-4f88-4944-8355-de30f80b51d9"
 };
+
 var loader = function () {
-window.Mash.init();
+    window.Mash.init();
 };
+
 var script = document.createElement("script");
 script.type = "text/javascript";
 script.defer = true;
 script.onload = loader;
 script.src = "https://app.mash.com/sdk/sdk.js";
+
 var head = document.getElementsByTagName("head")[0];
 head.appendChild(script);
