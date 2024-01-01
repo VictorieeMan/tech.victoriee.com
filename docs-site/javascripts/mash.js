82cd0bf68e801468a49e67da25a9addf937ef637
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2024-01-01 21:26:28 +0100

    fixed mash script

--- docs-site/javascripts/mash.js
+++ docs-site/javascripts/mash.js
@@ -3,15 +3,20 @@ window.MashSettings = {
   id: "56fe16a5-4f88-4944-8355-de30f80b51d9"
 };
 
-var loader = function () {
-  window.Mash.init();
-};
+function addMashScript() {
+  var loader = function () {
+      window.Mash.init();
+  };
 
-var script = document.createElement("script");
-script.type = "text/javascript";
-script.defer = true;
-script.onload = loader;
-script.src = "https://app.mash.com/sdk/sdk.js";
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.defer = true;
+  script.onload = loader;
+  script.src = "https://app.mash.com/sdk/sdk.js";
 
-var head = document.getElementsByTagName("head")[0];
-head.appendChild(script);
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(script);
+}
+
+// Subscribe to the document$ observable to run addMashScript each time a new page is loaded
+document$.subscribe(addMashScript);
