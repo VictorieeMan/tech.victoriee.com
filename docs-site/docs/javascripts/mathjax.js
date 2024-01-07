70032865724affd4cd50806d2a312805708b4f54
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2024-01-07 19:22:14 +0100

    added docs link in comment

--- docs-site/docs/javascripts/mathjax.js
+++ docs-site/docs/javascripts/mathjax.js
@@ -1,17 +1,21 @@
+/*
+Docs: arithmatex
+https://facelessuser.github.io/pymdown-extensions/extensions/arithmatex/#loading-mathjax-script---mathjax-3
+*/
+
 window.MathJax = {
-    tex: {
-      inlineMath: [["\\(", "\\)"]],
-      displayMath: [["\\[", "\\]"]],
-      processEscapes: true,
-      processEnvironments: true
-    },
-    options: {
-      ignoreHtmlClass: ".*|",
-      processHtmlClass: "arithmatex"
-    }
-  };
-  
-  document$.subscribe(() => {
-    MathJax.typesetPromise()
-  })
-  
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
